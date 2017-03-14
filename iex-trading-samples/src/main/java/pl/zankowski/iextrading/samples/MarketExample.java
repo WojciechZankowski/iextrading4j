45d68b404833e1fc94f4b100d4f223bdb7ffa587
Wojciech Zankowski <wza@empirica.pl>
2017-03-14 17:50:51 +0100

    add request filter for market volume

--- iex-trading-samples/src/main/java/pl/zankowski/iextrading/samples/MarketExample.java
+++ iex-trading-samples/src/main/java/pl/zankowski/iextrading/samples/MarketExample.java
@@ -1,5 +1,6 @@
 package pl.zankowski.iextrading.samples;
 
+import pl.zankowski.iextrading.api.filter.RequestFilter;
 import pl.zankowski.iextrading.api.market.MarketVolume;
 import pl.zankowski.iextrading.rest.client.IEXTradingClient;
 
@@ -14,10 +15,20 @@ public class MarketExample {
         IEXTradingClient iexTradingClient = IEXTradingClient.create();
 
         requestMarketsVolume(iexTradingClient);
+        requestMarketsVolumeWithRequestFilter(iexTradingClient);
     }
 
     private static void requestMarketsVolume(IEXTradingClient iexTradingClient) {
         MarketVolume[] marketsVolume = iexTradingClient.getMarketEndpoint().requestMarketVolume();
+        Arrays.stream(marketsVolume).forEach(System.out::println);
+    }
+
+    private static void requestMarketsVolumeWithRequestFilter(IEXTradingClient iexTradingClient) {
+        RequestFilter requestFilter = RequestFilter.builder()
+                .with("mic")
+                .with("marketPercent")
+                .build();
+        MarketVolume[] marketsVolume = iexTradingClient.getMarketEndpoint().requestMarketVolume(requestFilter);
         Arrays.stream(marketsVolume).forEach(System.out::println);
     }
 
