54d488cdfd274d5a0bb2902d2f45e7b4e7918840
Wojciech Zankowski <info@zankowski.pl>
2019-10-11 23:43:08 +0200

    Add technical indicator endpoint

---
+++ iextrading4j-client/src/main/java/pl/zankowski/iextrading4j/client/rest/request/stocks/v1/TechnicalIndicatorRequestBuilder.java
@@ -0,0 +1,70 @@
+package pl.zankowski.iextrading4j.client.rest.request.stocks.v1;
+
+import com.google.common.collect.Maps;
+import pl.zankowski.iextrading4j.api.stocks.ChartRange;
+import pl.zankowski.iextrading4j.api.stocks.v1.TechnicalIndicator;
+import pl.zankowski.iextrading4j.api.stocks.v1.TechnicalIndicatorType;
+import pl.zankowski.iextrading4j.client.rest.manager.RestRequest;
+import pl.zankowski.iextrading4j.client.rest.manager.RestRequestBuilder;
+import pl.zankowski.iextrading4j.client.rest.request.IEXCloudV1RestRequest;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.AbstractStocksRequestBuilder;
+
+import javax.ws.rs.core.GenericType;
+import java.math.BigDecimal;
+import java.util.Map;
+
+public class TechnicalIndicatorRequestBuilder extends AbstractStocksRequestBuilder<TechnicalIndicator,
+        TechnicalIndicatorRequestBuilder> implements IEXCloudV1RestRequest<TechnicalIndicator> {
+
+    private static final String RANGE_PARAM_NAME = "range";
+    private static final String INPUT_1_NAME = "input1";
+    private static final String INPUT_2_NAME = "input2";
+    private static final String INPUT_3_NAME = "input3";
+    private static final String INPUT_4_NAME = "input4";
+    private static final String TECHNICAL_INDICATOR_TYPE_NAME = "indicator";
+
+    private final Map<String, String> queryParameters = Maps.newHashMap();
+    private TechnicalIndicatorType indicatorType;
+
+    public TechnicalIndicatorRequestBuilder withTechnicalIndicatorType(final TechnicalIndicatorType indicatorType) {
+        this.indicatorType = indicatorType;
+        return this;
+    }
+
+    public TechnicalIndicatorRequestBuilder withInput1(final BigDecimal input1) {
+        this.queryParameters.put(INPUT_1_NAME, input1.toPlainString());
+        return this;
+    }
+
+    public TechnicalIndicatorRequestBuilder withInput2(final BigDecimal input2) {
+        this.queryParameters.put(INPUT_2_NAME, input2.toPlainString());
+        return this;
+    }
+
+    public TechnicalIndicatorRequestBuilder withInput3(final BigDecimal input3) {
+        this.queryParameters.put(INPUT_3_NAME, input3.toPlainString());
+        return this;
+    }
+
+    public TechnicalIndicatorRequestBuilder withInput4(final BigDecimal input4) {
+        this.queryParameters.put(INPUT_4_NAME, input4.toPlainString());
+        return this;
+    }
+
+    public TechnicalIndicatorRequestBuilder withRange(final ChartRange chartRange) {
+        queryParameters.put(RANGE_PARAM_NAME, chartRange.getCode());
+        return this;
+    }
+
+    @Override
+    public RestRequest<TechnicalIndicator> build() {
+        return RestRequestBuilder.<TechnicalIndicator>builder()
+                .withPath("/stock/{symbol}/indicator/{indicator}")
+                .addPathParam(SYMBOL_PARAM_NAME, getSymbol())
+                .addPathParam(TECHNICAL_INDICATOR_TYPE_NAME, indicatorType.name().toLowerCase()).get()
+                .withResponse(new GenericType<TechnicalIndicator>() {})
+                .addQueryParam(queryParameters)
+                .build();
+    }
+
+}
---
+++ iextrading4j-test/src/test/java/pl/zankowski/iextrading4j/test/rest/v1/stock/TechnicalIndicatorServiceTest.java
@@ -0,0 +1,68 @@
+package pl.zankowski.iextrading4j.test.rest.v1.stock;
+
+import org.junit.Test;
+import pl.zankowski.iextrading4j.api.stocks.Chart;
+import pl.zankowski.iextrading4j.api.stocks.ChartRange;
+import pl.zankowski.iextrading4j.api.stocks.v1.Split;
+import pl.zankowski.iextrading4j.api.stocks.v1.TechnicalIndicator;
+import pl.zankowski.iextrading4j.api.stocks.v1.TechnicalIndicatorType;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.SplitsRange;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.v1.SplitsRequestBuilder;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.v1.TechnicalIndicatorRequestBuilder;
+import pl.zankowski.iextrading4j.test.rest.v1.BaseIEXCloudV1ServiceTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TechnicalIndicatorServiceTest extends BaseIEXCloudV1ServiceTest {
+
+    @Test
+    public void technicalIndicatorServiceTest() {
+        stubFor(get(urlEqualTo(extendedPath("/stock/AAPL/indicator/bbands?range=6m")))
+                .withHeader("Accept", equalTo("application/json"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Accept", "application/json")
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("rest/v1/stock/TechnicalIndicatorResponse.json")));
+
+        final TechnicalIndicator technicalIndicator = cloudClient.executeRequest(new TechnicalIndicatorRequestBuilder()
+                .withSymbol("AAPL")
+                .withTechnicalIndicatorType(TechnicalIndicatorType.BBANDS)
+                .withRange(ChartRange.SIX_MONTHS)
+                .build());
+
+        assertThat(technicalIndicator.getIndicator()).hasSize(1);
+        assertThat(technicalIndicator.getIndicator().get(0)).hasSize(2);
+        assertThat(technicalIndicator.getIndicator().get(0).get(0)).isEqualTo(BigDecimal.valueOf(32.89100833109358));
+        assertThat(technicalIndicator.getIndicator().get(0).get(1)).isEqualTo(BigDecimal.valueOf(34.12321658001891));
+
+        assertThat(technicalIndicator.getChart()).hasSize(1);
+
+        final Chart chart = technicalIndicator.getChart().get(0);
+        assertThat(chart.getDate()).isEqualTo("2019-04-11");
+        assertThat(chart.getOpen()).isEqualTo(BigDecimal.valueOf(34.83));
+        assertThat(chart.getClose()).isEqualTo(BigDecimal.valueOf(35.42));
+        assertThat(chart.getHigh()).isEqualTo(BigDecimal.valueOf(36.41));
+        assertThat(chart.getLow()).isEqualTo(BigDecimal.valueOf(34.61));
+        assertThat(chart.getVolume()).isEqualTo(BigDecimal.valueOf(11104786));
+        assertThat(chart.getuOpen()).isEqualTo(BigDecimal.valueOf(34.76));
+        assertThat(chart.getuClose()).isEqualTo(BigDecimal.valueOf(36.16));
+        assertThat(chart.getuHigh()).isEqualTo(BigDecimal.valueOf(36.36));
+        assertThat(chart.getuLow()).isEqualTo(BigDecimal.valueOf(35.99));
+        assertThat(chart.getuVolume()).isEqualTo(BigDecimal.valueOf(11477825L));
+        assertThat(chart.getChange()).isEqualTo(BigDecimal.ZERO);
+        assertThat(chart.getChangePercent()).isEqualTo(BigDecimal.ZERO);
+        assertThat(chart.getLabel()).isEqualTo("Apr 11, 19");
+        assertThat(chart.getChangeOverTime()).isEqualTo(BigDecimal.ZERO);
+    }
+
+}
