535cb3cd1af0fa8b901744d34876c324ba898b32
xi chen <xchen@tibco.com>
2019-10-11 14:07:28 -0400

    change test

--- iextrading4j-client/src/test/java/pl/zankowski/iextrading4j/client/sse/request/alternative/SentimentSseRequestBuilderTest.java
+++ iextrading4j-client/src/test/java/pl/zankowski/iextrading4j/client/sse/request/alternative/SentimentSseRequestBuilderTest.java
@@ -1,7 +1,7 @@
 package pl.zankowski.iextrading4j.client.sse.request.alternative;
 
 import org.junit.Test;
-import pl.zankowski.iextrading4j.api.alternative.Sentiment;
+import pl.zankowski.iextrading4j.api.alternative.SentimentEvent;
 import pl.zankowski.iextrading4j.client.sse.manager.SseRequest;
 
 import javax.ws.rs.core.GenericType;
@@ -17,12 +17,12 @@ public class SentimentSseRequestBuilderTest {
     public void shouldSuccessfullyCreateRequest() {
         final String symbol = "IBM";
 
-        final SseRequest<List<Sentiment>> request = new SentimentSseRequestBuilder()
+        final SseRequest<List<SentimentEvent>> request = new SentimentSseRequestBuilder()
                 .withSymbol(symbol)
                 .build();
 
         assertThat(request.getPath()).isEqualTo("/sentiment");
-        assertThat(request.getResponseType()).isEqualTo(new GenericType<List<Sentiment>>() {});
+        assertThat(request.getResponseType()).isEqualTo(new GenericType<List<SentimentEvent>>() {});
         assertThat(request.getPathParams()).isEmpty();
         assertThat(request.getQueryParams()).contains(entry("nosnapshot", "false"), entry("symbols", symbol));
     }
