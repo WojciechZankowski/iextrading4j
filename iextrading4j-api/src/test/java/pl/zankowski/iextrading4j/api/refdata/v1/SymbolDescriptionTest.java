f1ab9a1bdf6c0c8852fcd7784fc8954decad8f31
Wojciech Zankowski <info@zankowski.pl>
2021-05-15 14:47:13 +0200

    Update search prod model (#118)
    * Adjust symbol description model
    
    * Update dependencies
    
    * Release 3.4.4

--- iextrading4j-api/src/test/java/pl/zankowski/iextrading4j/api/refdata/v1/SymbolDescriptionTest.java
+++ iextrading4j-api/src/test/java/pl/zankowski/iextrading4j/api/refdata/v1/SymbolDescriptionTest.java
@@ -15,23 +15,38 @@ public class SymbolDescriptionTest {
     @Test
     void constructor() {
         final String symbol = fixture.create(String.class);
+        final String exchange = fixture.create(String.class);
+        final String exchangeSuffix = fixture.create(String.class);
+        final String exchangeName = fixture.create(String.class);
+        final String name = fixture.create(String.class);
+        final String type = fixture.create(String.class);
+        final String iexId = fixture.create(String.class);
+        final String region = fixture.create(String.class);
+        final String currency = fixture.create(String.class);
+        final String figi = fixture.create(String.class);
         final String cik = fixture.create(String.class);
+        final String lei = fixture.create(String.class);
         final String securityName = fixture.create(String.class);
         final SymbolType securityType = fixture.create(SymbolType.class);
-        final String region = fixture.create(String.class);
-        final String exchange = fixture.create(String.class);
         final String sector = fixture.create(String.class);
-        final String currency = fixture.create(String.class);
 
-        final SymbolDescription symbolDescription = new SymbolDescription(symbol, cik, securityName,
-                securityType, region, exchange, sector, currency);
+        final SymbolDescription symbolDescription = new SymbolDescription(symbol, exchange, exchangeSuffix,
+                exchangeName, name, type, iexId, region, currency, figi, cik, lei, securityName, securityType,
+                sector);
 
         assertThat(symbolDescription.getSymbol()).isEqualTo(symbol);
+        assertThat(symbolDescription.getExchange()).isEqualTo(exchange);
+        assertThat(symbolDescription.getExchangeSuffix()).isEqualTo(exchangeSuffix);
+        assertThat(symbolDescription.getExchangeName()).isEqualTo(exchangeName);
+        assertThat(symbolDescription.getName()).isEqualTo(name);
+        assertThat(symbolDescription.getType()).isEqualTo(type);
+        assertThat(symbolDescription.getIexId()).isEqualTo(iexId);
+        assertThat(symbolDescription.getFigi()).isEqualTo(figi);
         assertThat(symbolDescription.getCik()).isEqualTo(cik);
+        assertThat(symbolDescription.getLei()).isEqualTo(lei);
         assertThat(symbolDescription.getSecurityName()).isEqualTo(securityName);
         assertThat(symbolDescription.getSecurityType()).isEqualTo(securityType);
         assertThat(symbolDescription.getRegion()).isEqualTo(region);
-        assertThat(symbolDescription.getExchange()).isEqualTo(exchange);
         assertThat(symbolDescription.getSector()).isEqualTo(sector);
         assertThat(symbolDescription.getCurrency()).isEqualTo(currency);
     }
